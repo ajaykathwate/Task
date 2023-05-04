8fdc7675203087e27c3e871fcd5cd5988c9dd0b9
Ajay Kathwate <ajaykathwate13@gmail.com>
2023-05-05 00:17:52 +0530

    Task 2 added

---
+++ Test.java
@@ -0,0 +1,31 @@
+public class Demo {
+	
+	public static int fly(int[] nums) {
+		if(nums.length == 1){
+            return 0;
+        }
+        int max = 0;
+        int curr = 0;
+        int count = 0;
+        for(int i = 0 ; i < nums.length - 1 ; i++){
+            max = Math.max(max , i + nums[i]);
+            if(curr == i){
+                curr = max;
+                count++;
+            }
+            if(curr>nums.length-1){
+                return count;
+            }
+        }
+        return count;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {1,6,3,4,5,0,0,0,6};
+		int ans = fly(nums);
+		System.out.println(ans);
+		
+	}
+
+}
